5c7df034ab2f2a031364fe93d82ac696046ba8bd
mojave2 <0109chenchen@gmail.com>
2023-05-11 17:53:45 +0800

    Simplest SQL Compiler and VM

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use std::io::Write;
 
 fn main() {
-    let mut input_buffer = InputBuffer::new();
     loop {
+        let mut input_buffer = InputBuffer::new();
         print_prompt();
         read_input(&mut input_buffer);
 
-        match input_buffer.as_ref() {
-            ".exit" => break,
-            s => println!("Unrecognized command {s:?}.")
+        if input_buffer.starts_with('.') {
+            match do_meta_command(&input_buffer) {
+                Ok(_) => continue,
+                Err(MetaCmdErr::Unrecognized) => {
+                    println!("Unrecognized command {input_buffer:?}.");
+                    continue;
+                }
+            }
         }
-        input_buffer.clear();
+
+        let statement = match prepare_statement(&input_buffer) {
+            Ok(stmt) => stmt,
+            Err(_) => {
+                println!("Unrecognized keyword at start of {input_buffer:?}.");
+                continue;
+            }
+        };
+
+        execute_statement(&statement);
+
+        println!("Executed.");
     }
 }
 
@@ -30,5 +46,42 @@ fn read_input(input_buffer: &mut InputBuffer) {
             println!("Error reading input!");
             std::process::exit(1);
         }
+    }
+}
+
+enum MetaCmdErr {
+    Unrecognized,
+}
+
+fn do_meta_command(input: &InputBuffer) -> Result<(), MetaCmdErr> {
+    match input.as_ref() {
+        ".exit" => std::process::exit(0),
+        _ => return Err(MetaCmdErr::Unrecognized),
+    }
+    Ok(())
+}
+
+enum Statement {
+    Insert,
+    Select,
+}
+
+enum PrepareErr {
+    Unrecognized,
+}
+
+fn prepare_statement(input: &InputBuffer) -> Result<Statement, PrepareErr> {
+    match input {
+        s if s.starts_with("insert") => Ok(Statement::Insert),
+        s if s.starts_with("select") => Ok(Statement::Select),
+        _ => Err(PrepareErr::Unrecognized)
+    }
+}
+
+fn execute_statement(stmt: &Statement) {
+    use Statement::*;
+    match stmt {
+        Insert => println!("This is where we would do an insert."),
+        Select => println!("This is where we would do a select."),
     }
 }
