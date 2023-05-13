37259545c02721906aef6453176bfc4bfeb98a49
mojave2 <0109chenchen@gmail.com>
2023-05-13 16:19:31 +0800

    new test: max-length-string-field

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,15 +3,17 @@ use predicates::prelude::predicate;
 
 #[test]
 fn insert_and_retrieve_a_row() {
+    let username = "foo";
+    let email = "foo@bar.com";
     let mut cmd = Command::cargo_bin("sqlite_rs").unwrap();
     let assert = cmd
-        .write_stdin(["insert 0 foo foo@bar.com", "select", ".exit"].join("\n"))
+        .write_stdin([&format!("insert 0 {username} {email}"), "select", ".exit"].join("\n"))
         .assert();
 
     assert.success().stdout(
         [
             "db > Executed.",
-            "db > (0, \"foo\", \"foo@bar.com\")",
+            &format!("db > (0, {username:?}, {email:?})"),
             "Executed.",
             "db > ",
         ]
@@ -22,12 +24,39 @@ fn insert_and_retrieve_a_row() {
 #[test]
 fn table_is_full() {
     let mut cmd = Command::cargo_bin("sqlite_rs").unwrap();
-    let input: String = (0..1401)
+    let script: String = (0..1401)
         .map(|i| format!("insert {i} user{i} person{i}@example.com\n"))
         .collect();
-    let assert = cmd.write_stdin(input + ".exit").assert();
+    let assert = cmd.write_stdin(script + ".exit").assert();
 
     assert
         .success()
         .stdout(predicate::str::contains("Error: Table full."));
+}
+
+#[test]
+fn allows_insert_strings_of_maximum_length() {
+    let long_username = ["a"; 32].join("");
+    let long_email = ["a"; 255].join("");
+    let mut cmd = Command::cargo_bin("sqlite_rs").unwrap();
+    let assert = cmd
+        .write_stdin(
+            [
+                &format!("insert 0 {long_username} {long_email}"),
+                "select",
+                ".exit",
+            ]
+            .join("\n"),
+        )
+        .assert();
+
+    assert.success().stdout(
+        [
+            "db > Executed.",
+            &format!("db > (0, {long_username:?}, {long_email:?})"),
+            "Executed.",
+            "db > ",
+        ]
+        .join("\n"),
+    );
 }
